441e3795332f1ab87cdcb026ace7cb6d0cdfefc3
DevOps <andersontsopze+code@gmail.com>
2023-10-17 12:30:31 -0400

    second commit

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return 'Hello, World! This is my first Flask app. I am very excited about thi journey.'
+	return 'Hello, World! This is my first Flask app.\
+ I am very excited about thi journey.'
 
 if __name__ == '__main__':
 	app.run(debug=True)
