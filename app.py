dcf6588f207fbbb850e1fc6ea53a75f1a57e8536
x0atsopze <109428762+x0atsopze@users.noreply.github.com>
2024-09-18 09:25:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return 'Hello, World! This is my first Flask app. \
-I am very excited about this journey.'
+	return 'Hello, World! This is my first Flask app.'
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
